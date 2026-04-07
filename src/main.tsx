bb6aa0786d2826a959d847fe8332e712e0784df7
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-04-07 07:01:02 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BootDiagnostics } from "./components/BootDiagnostics";
-import { canBoot } from "./env";
 
-const el = document.getElementById("root");
-
-if (!el) {
-  // If root element is missing, fail loudly (helps in debugging broken HTML)
-  throw new Error("Root element #root not found");
-}
-
-createRoot(el).render(
-  <React.StrictMode>
-    {canBoot() ? <App /> : <BootDiagnostics />}
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
