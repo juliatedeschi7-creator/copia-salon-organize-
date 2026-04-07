c63d22d6f581fe27b4dbe9f05b0228016ee13359
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-04-07 07:00:29 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,2 @@
-/// <reference types="vite/client" />
 
-declare const __BUILD_SHA__: string;
-declare const __VERCEL_ENV__: string;
-declare const __VERCEL_URL__: string;
+/// <reference types="vite/client" />
