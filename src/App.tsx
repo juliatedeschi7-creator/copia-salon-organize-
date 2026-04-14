81f087af2ac91b58a03e9d8377f18a90d931d847
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-04-14 10:37:35 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
-
-import { AuthProvider } from "@/contexts/AuthContext";
-import { SalonProvider } from "@/contexts/SalonContext";
-
-import AppRoutes from "@/AppRoutes";
-
-const queryClient = new QueryClient();
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <SalonProvider>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <AppRoutes />
-            </BrowserRouter>
-          </TooltipProvider>
-        </SalonProvider>
-      </AuthProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>APP ABRIU SEM AUTH</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
