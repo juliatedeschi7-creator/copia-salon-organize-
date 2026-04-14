5f13df38b0a8e63e20593d8b6fe496b9a0b2b79f
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-04-14 10:26:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+
+import { AuthProvider } from "@/contexts/AuthContext";
+import { SalonProvider } from "@/contexts/SalonContext";
+
+import AppRoutes from "@/AppRoutes";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>APP OK</div>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <SalonProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <AppRoutes />
+            </BrowserRouter>
+          </TooltipProvider>
+        </SalonProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
