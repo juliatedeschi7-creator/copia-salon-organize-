4a41961ea13086f7a532deefb2dcc84b2829eff1
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-04-14 14:44:38 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { SalonProvider } from "@/contexts/SalonContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<div>AUTH OK</div>} />
-        </Routes>
-      </BrowserRouter>
+      <SalonProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>AUTH + SALON OK</div>} />
+          </Routes>
+        </BrowserRouter>
+      </SalonProvider>
     </AuthProvider>
   );
 }
