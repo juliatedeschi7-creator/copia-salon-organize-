dad6f9e2bc4e9501658efaaca4d3cf1c51a36f1e
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-04-14 23:07:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AuthProvider } from "@/contexts/AuthContext";
 import { SalonProvider } from "@/contexts/SalonContext";
 import AppRoutes from "@/AppRoutes";
 
@@ -9,18 +10,16 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* 🔥 DESLIGA TEMPORARIAMENTE */}
-      {/* <AuthProvider> */}
-
-      <SalonProvider>
-        <TooltipProvider>
-          <BrowserRouter>
-            <AppRoutes />
-          </BrowserRouter>
-        </TooltipProvider>
-      </SalonProvider>
-
-      {/* </AuthProvider> */}
+      <AuthProvider>
+        <SalonProvider>
+          <TooltipProvider>
+            <BrowserRouter>
+              <AppRoutes />
+            </BrowserRouter>
+          </TooltipProvider>
+        </SalonProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
+
